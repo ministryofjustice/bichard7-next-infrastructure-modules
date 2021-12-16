8dc43339968eccfb302f1af1cfd7578eef2a82e0
Brett Minnie <github@bdb-studios.co.uk>
2021-12-16 15:22:48 +0000

    Imported codebuild base resources

---
+++ modules/codebuild_base_resources/data.tf
@@ -0,0 +1,74 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "template_file" "allow_sns_publish_policy" {
+  template = file("${path.module}/policies/allow_sns_policy.json.tpl")
+
+  vars = {
+    sns_topic_arn = aws_sns_topic.build_notifications.arn
+    account_id    = data.aws_caller_identity.current.account_id
+  }
+}
+
+data "template_file" "allow_scanning_sns_publish_policy" {
+  template = file("${path.module}/policies/allow_sns_policy.json.tpl")
+
+  vars = {
+    sns_topic_arn = aws_sns_topic.scanning_notifications.arn
+    account_id    = data.aws_caller_identity.current.account_id
+  }
+}
+
+data "template_file" "allow_codestar_kms" {
+  template = file("${path.module}/policies/allow_codestar_kms.json.tpl")
+
+  vars = {
+    region     = data.aws_region.current.name
+    account_id = data.aws_caller_identity.current.account_id
+  }
+}
+
+data "aws_ssm_parameter" "slack_webhook" {
+  name            = aws_ssm_parameter.slack_webhook.name
+  with_decryption = true
+}
+
+data "template_file" "webhook_source" {
+  template = file("${path.module}/source/webhook.py.tpl")
+
+  vars = {
+    webhook_url  = data.aws_ssm_parameter.slack_webhook.value
+    channel_name = var.notifications_channel_name
+  }
+}
+
+data "template_file" "scanning_webhook_source" {
+  template = file("${path.module}/source/scanning_webhook.py.tpl")
+
+  vars = {
+    webhook_url  = data.aws_ssm_parameter.slack_webhook.value
+    channel_name = var.notifications_channel_name
+  }
+}
+
+data "template_file" "child_accounts_cmk_access_template" {
+  template = file("${path.module}/policies/child_accounts_cmk_access.json.tpl")
+
+  vars = {
+    child_accounts = jsonencode(var.allow_accounts)
+    account_id     = data.aws_caller_identity.current.account_id
+  }
+}
+
+data "aws_iam_user" "ci_user" {
+  user_name = "cjse.ci"
+}
+
+data "template_file" "allow_ci_slack_ssm" {
+  template = file("${path.module}/policies/allow_ci_ssm.json.tpl")
+
+  vars = {
+    slack_webhook_arn = aws_ssm_parameter.slack_webhook.arn
+  }
+}
