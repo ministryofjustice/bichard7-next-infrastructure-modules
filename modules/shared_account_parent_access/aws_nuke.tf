a480bd36acfc11f95ae602d6c038c8083570d76b
Brett Minnie <github@bdb-studios.co.uk>
2022-01-07 14:11:52 +0000

    Fixed typo, corrected attach group

--- modules/shared_account_parent_access/aws_nuke.tf
+++ modules/shared_account_parent_access/aws_nuke.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_policy" "allow_assume_aws_nuke_access_role" {
   count  = (var.aws_nuke_access_arn != null) ? 1 : 0
-  name   = "Assume-AWS-Nume-Access-on-${var.child_account_id}"
+  name   = "Assume-AWS-Nuke-Access-on-${var.child_account_id}"
   policy = data.template_file.allow_assume_aws_nuke_access[count.index].rendered
 }
 
 resource "aws_iam_group_policy_attachment" "aws_nuke_access_policy_attachment" {
   count      = (var.aws_nuke_access_arn != null) ? 1 : 0
   policy_arn = aws_iam_policy.allow_assume_aws_nuke_access_role[count.index].arn
-  group      = var.ci_access_group_name
+  group      = data.aws_iam_group.nuke_user_group[count.index].group_name
 }
