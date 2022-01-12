261a55415f45e13e73738b14c47698b18e9b8e9b
Brett Minnie <github@bdb-studios.co.uk>
2022-01-12 14:14:05 +0000

    Updated mail record

--- modules/postfix_vpc/dns.tf
+++ modules/postfix_vpc/dns.tf
@@ -1,11 +1,8 @@
 resource "aws_route53_record" "mail" {
   name    = "mail.${data.aws_route53_zone.public.name}"
-  type    = "A"
+  type    = "CNAME"
   zone_id = data.aws_route53_zone.public.zone_id
   ttl     = 60
 
-  records = concat(
-    aws_network_interface.static_ip.*.private_ip,
-    aws_eip.postfix_static_ip.*.public_ip
-  )
+  records = module.postfix_nlb.load_balancer.dns_name
 }
