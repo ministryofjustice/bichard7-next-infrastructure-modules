a85dccfa79b08b7b4ac8699e3f34f787ce54c6d7
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-02-24 13:08:25 +0000

    fix: 🐛 conditionally create slack lambda

--- modules/lambda_slack_webhook/data.tf
+++ modules/lambda_slack_webhook/data.tf
@@ -3,17 +3,19 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_ssm_parameter" "slack_webhook" {
+  count           = var.is_production ? 1 : 0
   name            = "/lambda/slack/webhook"
   with_decryption = true
 }
 
 data "archive_file" "slack_webhook_notification" {
+  count       = var.is_production ? 1 : 0
   output_path = "/tmp/slack_webhook_notification_rule.zip"
   type        = "zip"
 
   source {
     content = templatefile("${path.module}/source/webhook.py.tpl", {
-      webhook_url                = data.aws_ssm_parameter.slack_webhook.value
+      webhook_url                = data.aws_ssm_parameter.slack_webhook[count.index].value
       notifications_channel_name = var.notifications_channel_name
     })
     filename = "webhook.py"
