3089db3f94b8647ff47a3fcd626d9919668fa486
jaskaran <jaskaran.sarkaria@googlemail.com>
2022-05-04 11:51:13 +0100

    fix: 🐛 opensearch provider version

--- modules/opensearch/providers.tf
+++ modules/opensearch/providers.tf
@@ -2,10 +2,11 @@ provider "elasticsearch" {
   url      = "https://${aws_elasticsearch_domain.os.endpoint}"
   username = local.os_user_name
   #password              = jsondecode(aws_secretsmanager_secret_version.os_password.secret_string)
-  password              = aws_ssm_parameter.es_password.value
-  sniff                 = false
-  aws_region            = data.aws_region.current.name
-  healthcheck           = false
-  elasticsearch_version = aws_elasticsearch_domain.os.elasticsearch_version
+  password    = aws_ssm_parameter.es_password.value
+  sniff       = false
+  aws_region  = data.aws_region.current.name
+  healthcheck = false
+  # elasticsearch_version = aws_elasticsearch_domain.os.elasticsearch_version
+  elasticsearch_version = "OpenSearch_1.0"
   sign_aws_requests     = false
 }
