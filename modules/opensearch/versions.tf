34c1eacc783735719315d228442ea145d810ba6a
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-05-10 16:08:44 +0100

    fix: 🐛 update es provider (#104)

--- modules/opensearch/versions.tf
+++ modules/opensearch/versions.tf
@@ -18,7 +18,7 @@ terraform {
     }
     elasticsearch = {
       source  = "phillbaker/elasticsearch"
-      version = "= 2.0.0-beta.4"
+      version = "= 2.0.1"
     }
     null = {
       source  = "hashicorp/null"
